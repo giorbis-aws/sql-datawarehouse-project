bee387e7a2dc2f9e0f9b50ff0d76e359df2ba006
giorbis-aws <giorbissmarin@gmail.com>
2026-01-16 10:55:42 -0300

    Update bulk_databases.sql

--- scripts/bulk_databases.sql
+++ scripts/bulk_databases.sql
@@ -11,9 +11,22 @@ Script Purpose:
 */
 
 
+
 CREATE or ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+    PRINT '==========================================================';
+    PRINT 'Loading Bronze Layer';
+    PRINT '==========================================================';
+    PRINT '----------------------------------------------------------';
+    PRINT 'Loading CRM Tables';
+    PRINT '----------------------------------------------------------';
+
+    PRINT '>> Truncating Table: bronze.crm_cust_info';
+
     TRUNCATE TABLE bronze.crm_cust_info;
+
+    PRINT '>> Inserting data into: bronze.crm_cust_info';
+
     BULK INSERT bronze.crm_cust_info
     from '/var/opt/mssql/data/source_crm/cust_info.csv'
     WITH (
@@ -23,7 +36,12 @@ BEGIN
         TABLOCK
     );   
 
+    PRINT '>> Truncating Table: bronze.crm_sales_details';
+
     TRUNCATE TABLE bronze.crm_sales_details;
+
+    PRINT '>> Inserting data into: bronze.crm_sales_details';
+
     BULK INSERT bronze.crm_sales_details
     from '/var/opt/mssql/data/source_crm/sales_details.csv'
     WITH (
@@ -33,7 +51,12 @@ BEGIN
         TABLOCK
     );    
 
+    PRINT '>> Truncating Table: bronze.crm_prd_info';
+
     TRUNCATE TABLE bronze.crm_prd_info;
+
+    PRINT '>> Inserting data into: bronze.crm_prd_info';
+
     BULK INSERT bronze.crm_prd_info
     from '/var/opt/mssql/data/source_crm/prd_info.csv'
     WITH (
@@ -43,7 +66,16 @@ BEGIN
         TABLOCK
     ); 
 
+    PRINT '----------------------------------------------------------';
+    PRINT 'Loading ERP Tables';
+    PRINT '----------------------------------------------------------';
+
+    PRINT '>> Truncating Table: bronze.erp_cust_az12';
+
     TRUNCATE TABLE bronze.erp_cust_az12;
+
+    PRINT '>> Inserting data into: bronze.erp_cust_az12';
+
     BULK INSERT bronze.erp_cust_az12
     from '/var/opt/mssql/data/source_erp/cust_az12.csv'
     WITH (
@@ -53,7 +85,12 @@ BEGIN
         TABLOCK
     );
 
+    PRINT '>> Truncating Table: bronze.erp_loc_a101';
+
     TRUNCATE TABLE bronze.erp_loc_a101;
+
+    PRINT '>> Inserting data into: bronze.erp_loc_a101';
+
     BULK INSERT bronze.erp_loc_a101
     from '/var/opt/mssql/data/source_erp/loc_a101.csv'
     WITH (
@@ -63,7 +100,12 @@ BEGIN
         TABLOCK
     );
 
+    PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+
     TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+    PRINT '>> Inserting data into: bronze.erp_px_cat_g1v2';
+
     BULK INSERT bronze.erp_px_cat_g1v2
     from '/var/opt/mssql/data/source_erp/px_cat_g1v2.csv'
     WITH (
